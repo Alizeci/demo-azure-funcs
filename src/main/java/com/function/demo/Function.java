44d40d97d9ccfb9cf3dcfebbfae1944bdd404763
Alejandra Izquierdo <alizeci17@gmail.com>
2023-09-19 17:26:21 -0500

    [demo-monex-java] added service bus queue

--- src/main/java/com/function/demo/Function.java
+++ src/main/java/com/function/demo/Function.java
@@ -5,9 +5,11 @@ import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueOutput;
 
 import java.util.Optional;
 
@@ -15,29 +17,21 @@ import java.util.Optional;
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
-    @FunctionName("HttpExample")
+
+    @FunctionName("sendMessage")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
+        @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+        @ServiceBusQueueOutput(name = "outputMessage", queueName = "demo-monex-queue", connection = "AzureWebJobsServiceBus") OutputBinding<String> outputMessage,
+        final ExecutionContext context) {
+
         context.getLogger().info("Java HTTP trigger processed a request.");
+        final String message = request.getBody().orElse(null);
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
-
-        if (name == null) {
+        if (message == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            outputMessage.setValue(message);
+            return request.createResponseBuilder(HttpStatus.OK).body("Message processed: " + message).build();
         }
     }
 }
